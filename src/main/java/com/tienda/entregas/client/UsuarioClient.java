498cbea6526c26d16cb1a0cd701cca873039efe8
JuanPosso3 <juan.miguel.posso@correounivalle.edu.co>
2025-06-19 09:12:57 -0500

    Actualizacion

--- src/main/java/com/tienda/entregas/client/UsuarioClient.java
+++ src/main/java/com/tienda/entregas/client/UsuarioClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "usuarios-service", url = "${usuarios-service.url}")
+@FeignClient(name = "usuarios-service")
 public interface UsuarioClient {
 
     Logger logger = LoggerFactory.getLogger(UsuarioClient.class);
